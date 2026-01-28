d0965b98e3cf62788758513063f083ec2c0c7586
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2026-01-28 14:54:46 -0300

    feat: add Button component and enhance App layout with Tailwind CSS styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import Button from "./components/Button";
 import "./styles/main.css";
 
 function App() {
-	return <h1>hello</h1>;
+	return (
+		<div className="flex flex-col gap-4 bg-neutral-800 m-8 p-8 rounded-lg w-full max-w-md h-full">
+			<h1 className="text-neutral-100 title-lg">Make an appointment</h1>
+			<h2 className="text-neutral-300 title-md">
+				Select a date and time, plus inform the client name for the appointment.
+			</h2>
+			<Button>SUBMIT</Button>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,24 @@
+import clsx from "clsx";
+import type { ReactNode } from "react";
+
+type ButtonProps = {
+	children: ReactNode;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+function Button({ children, disabled, ...rest }: ButtonProps) {
+	return (
+		<button
+			{...rest}
+			className={clsx(
+				"flex justify-center items-center bg-primary hover:bg-primary-light px-4 py-2 border-2 hover:border-primary-light rounded-lg h-14 text-gray-950 transition-colors cursor-pointer title-sm",
+				{
+					disabled: "bg-primary-dark cursor-not-allowed",
+				},
+			)}
+		>
+			{children}
+		</button>
+	);
+}
+
+export default Button;
